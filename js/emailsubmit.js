e3e5dc305a151bee40df0cb0f9dac394d8b8f709
RYAN WONG <rmawong.92@gmail.com>
2016-02-07 23:47:22 -0800

    Handle trailing whitespace issue for email validation

--- js/emailsubmit.js
+++ js/emailsubmit.js
@@ -73,7 +73,7 @@
         },
         emailIsValid: function(email) {
             var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
+            return re.test(email.trim());
         },
         submitEmail: function () {
 			$('.signupButton').click(function(event) {
